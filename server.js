2a13211ca1c5a912cfc2c6a534a4f9084db8fca0
Felipe Johnny <felipijohnny@outlook.com>
2025-01-15 22:05:18 -0300

    primeiro commit

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const app = require("./app");
-const PORT = process.env.PORT || 3000;
+require("dotenv").config(); // Carrega as variáveis de ambiente do arquivo .env
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+const fs = require("fs");
+const https = require("https");
+const app = require("./src/app.js"); // Importa a aplicação Express
+
+const PORT = process.env.PORT; // Define a porta do servidor HTTPS
+
+// Configuração do HTTPS com os certificados SSL
+const httpsOptions = {
+  key: fs.readFileSync("/etc/letsencrypt/live/felipejohnny.com/privkey.pem"), // Caminho da chave privada
+  cert: fs.readFileSync("/etc/letsencrypt/live/felipejohnny.com/fullchain.pem"), // Caminho do certificado
+};
+
+// Criação do servidor HTTPS
+const server = https.createServer(httpsOptions, app);
+
+// Inicia o servidor
+server.listen(PORT, () => {
+  console.log(`Servidor HTTPS rodando na porta ${PORT}`);
 });
